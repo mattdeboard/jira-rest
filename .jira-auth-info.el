3966694a28abeae24dacc6464e737007d286db23
Matt DeBoard <matt.deboard@gmail.com>
2012-05-06 17:20:45 -0400

    Add comment to the dotfile.

--- .jira-auth-info.el
+++ .jira-auth-info.el
@@ -1,3 +1,5 @@
+;; Fill in these values with your JIRA auth/API information.
+;; These values will be loaded and used by the main module.
 (setq jira-username nil
       jira-password nil
       jira-rest-endpoint "")
